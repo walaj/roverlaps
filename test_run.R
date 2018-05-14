cd269c77bf8b051e80d2b74c4aee6571067030ec
Jeremiah Wala <jwala@broadinstitute.org>
2018-05-14 10:12:05 -0400

    fixed test suite

--- test_run.R
+++ test_run.R
@@ -2,6 +2,7 @@ library(roverlaps)
 library(gUtils)
 
 k=1e6
+cores=1
 verbose=FALSE
 o1 <- data.table(seqnames=factor(rep(c(1, "X"), each=k)), start=seq(k*2), end=seq(k*2)+2)
 k=1e8
@@ -9,7 +10,7 @@ o2 <- data.table(seqnames=factor(rep(c(1, "X"), each=k)), start=seq(k*2), end=se
 
 for (i in 1:5) {
   print(i)
-  system.time(o <- roverlaps(o1, o2, robust=robust, cores=cores, verbose=verbose, index_only=index_only))
+  system.time(o <- roverlaps(o1, o2, cores=cores, verbose=verbose, index_only=index_only))
   gc()
   Sys.sleep(15)
   system.time(o <- gr.findoverlaps(o1, o2, verbose=verbose, max.chunk=1e16))
--- tests/testthat/test-roverlaps.R
+++ tests/testthat/test-roverlaps.R
@@ -1,20 +1,20 @@
 context("test-roverlaps.R")
 library(data.table)
 test_that("unit test 1", {
-  k=1e6
-  verbose=TRUE
+  k=1e1
+  verbose=FALSE
   cores=1
   index_only=FALSE
   o1 <- data.table(seqnames=factor(rep(c(1, "X"), each=k)), start=seq(k*2), end=seq(k*2)+2)
-  k=1e6
+  k=1e1
   o2 <- data.table(seqnames=factor(rep(c(1, "X"), each=k)), start=seq(k*2), end=seq(k*2)+2)
-  system.time(o <- roverlaps(o1, o2, robust=robust, cores=cores, verbose=verbose, index_only=index_only))
-  system.time(o <- gr.findoverlaps(o1, o2, verbose=TRUE, max.chunk=1e16))
-  rm(d2)
-  expect_equal(as.character(o$seqnames), as.character("1"))
-  expect_equal(nrow(o), 1)
-  expect_equal(o$start, 5)
-  expect_equal(o$end, 9)
-  expect_equal(o$query.id, 1)
-  expect_equal(o$subject.id, 2)
+
+  oc <- roverlaps(o1, o2, cores=cores, verbose=verbose, index_only=index_only)
+
+  expect_equal(as.character(oc$seqnames)[1], as.character("1"))
+  expect_equal(nrow(oc), 88)
+  expect_equal(oc$start[1], 1)
+  expect_equal(oc$end[1], 3)
+  expect_equal(oc$query.id[1], 1)
+  expect_equal(oc$subject.id[1], 1)
 })
